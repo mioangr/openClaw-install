826eda4cae429ce35e1605ec9a61bf603c3a2b27
mioangr <mioan@hotmail.com>
2026-03-29 11:56:30 +0300

    Update setup_openclaw.sh to include HOW TO RUN THIS SCRIPT section

--- setup_openclaw.sh
+++ setup_openclaw.sh
@@ -1,9 +1,16 @@
-# This script ...
+#!/bin/bash
 
 # FEATURES
+# Some feature of the script
 
-# HOW TO RUN THIS SCRIPT
-# To download and run the script, use the following command:
-# curl -O https://raw.githubusercontent.com/mioangr/openClaw-install/main/setup_openclaw.sh && sudo bash setup_openclaw.sh
+# HOW TO RUN THIS SCRIPT:
+# To download and run the script in one command:
+#   curl -O https://raw.githubusercontent.com/mioangr/openClaw-install/main/setup_openclaw.sh && sudo bash setup_openclaw.sh
+# 
+# Or download first, then run:
+#   curl -O https://raw.githubusercontent.com/mioangr/openClaw-install/main/setup_openclaw.sh
+#   sudo bash setup_openclaw.sh
 
-# All other content in the file follows here...
+set -e
+
+# The rest of the script...
